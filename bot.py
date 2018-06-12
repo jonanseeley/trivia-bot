a780e6874f887ae7e98220adc29f64756b0b0f9f
Jonan Seeley <jseeley@psc.edu>
2018-06-11 21:51:53 -0400

    Updated, but still buggy version of bot

--- bot.py
+++ bot.py
@@ -15,34 +15,42 @@ class MyHandler(PatternMatchingEventHandler):
     patterns = ["*"]
 
     def process(self, event):
-        path = event.src_path
         # load the example image and convert it to grayscale
+        path = event.src_path
         image = cv2.imread(path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        resized = cv2.resize(image, (0, 0), fx=3.0, fy=3.0)
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        blur = cv2.GaussianBlur(thresh, (5, 5), 0)
 
         # write the grayscale image to disk as a temporary file so we can apply
         # OCR to it
         filename = "{}.png".format(os.getpid())
-        cv2.imwrite(filename, gray)
+        cv2.imwrite(filename, blur)
 
         # load the image as a PIL/Pillow image, apply OCR, then delete the temp
         text = pytesseract.image_to_string(Image.open(filename))
         text = [x.encode('ascii', 'ignore') for x in text.splitlines()]
         os.remove(filename)
+        print text
 
         # We want to get the question separate from the answers and other text
         # So, we can find the line that has a question mark, move back unit
-        r = re.compile(".*\?")
         try:
+            # Find the index where the question ends
+            r = re.compile(".*\?")
             newtext = filter(r.match, text)[0]
             end_of_q = text.index(newtext)
             index = end_of_q
+            # Find the index of the beginning of the question
             while index > 0 and text[index] != '':
                 index -= 1
             length_of_q = end_of_q - index + 1
-            newtext = [x for x in text[index:] if x != ''][:(length_of_q + 2)]
-            newtext[:-3] = [' '.join(newtext[:-3])]
+            # Remove any empty strings in the array
+            newtext = [x for x in text[index:] if x != '']
+            print newtext
+            newtext[:length_of_q] = [' '.join(newtext[:length_of_q])]
+            newtext = newtext[:(length_of_q + 2)]
             question = newtext[0]
             choices = newtext[1:]
             queries = [(question + " \"" + x + "\"") for x in choices]
