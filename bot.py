45d83fa5b343e75f70db10b73e8c1e59e3702604
Jonan Seeley <jseeley@psc.edu>
2018-08-16 20:16:10 -0400

    Functioning bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ class MyHandler(PatternMatchingEventHandler):
         # load the example image and convert it to grayscale
         path = event.src_path
         image = cv2.imread(path)
-        resized = cv2.resize(image, (0, 0), fx=3.0, fy=3.0)
+        cropped = image[200:650, 70:550]
+        resized = cv2.resize(cropped, (0, 0), fx=3.0, fy=3.0)
         gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
         blur = cv2.GaussianBlur(thresh, (5, 5), 0)
@@ -31,8 +32,7 @@ class MyHandler(PatternMatchingEventHandler):
         # load the image as a PIL/Pillow image, apply OCR, then delete the temp
         text = pytesseract.image_to_string(Image.open(filename))
         text = [x.encode('ascii', 'ignore') for x in text.splitlines()]
-        os.remove(filename)
-        print text
+        #os.remove(filename)
 
         # We want to get the question separate from the answers and other text
         # So, we can find the line that has a question mark, move back unit
@@ -48,20 +48,28 @@ class MyHandler(PatternMatchingEventHandler):
             length_of_q = end_of_q - index + 1
             # Remove any empty strings in the array
             newtext = [x for x in text[index:] if x != '']
-            print newtext
             newtext[:length_of_q] = [' '.join(newtext[:length_of_q])]
             newtext = newtext[:(length_of_q + 2)]
             question = newtext[0]
             choices = newtext[1:]
             queries = [(question + " \"" + x + "\"") for x in choices]
-            # We want to query each of a few search engines in a couple ways
-            # We want to query Google, DuckDuckGo, Bing
-            # Each one should be queried at least like question + "choice"
+            # Query Google to get number of results for each answer
             query_responses = [google.search(x, 1) for x in queries]
+            answers = {}
             for x in xrange(3):
-                print choices[x]+": "+str(query_responses[x][0].number_of_results)
-        except Exception:
+                answers[choices[x]] = query_responses[x][0].number_of_results
+            best_count = 0
+            best_answer = ""
+            for k,v in answers.items():
+                print (k + ": " + str(v))
+                if v > best_count:
+                    best_count = v
+                    best_answer = k
+            print "Choose " + best_answer + "!"
+
+        except Exception as e:
             print "Recognition failed!"
+            print e
 
     def on_created(self, event):
         self.process(event)
